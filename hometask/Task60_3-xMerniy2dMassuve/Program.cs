540336105201bf970a62d7551da6f3e7970d1e87
Grosere <144502806+Grosere@users.noreply.github.com>
2023-10-07 23:00:28 +0300

    add hometask

---
+++ hometask/Task60_3-xMerniy2dMassuve/Program.cs
@@ -0,0 +1,35 @@
+﻿/* 
+Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+*/
+
+using System;
+
+class MainClass 
+{
+public static void Main (string[] args)
+{
+int[, ,] threeDArray = new int[2, 2, 2];
+Random random = new Random();
+
+for (int i = 0; i < 2; i++)  
+{  
+  for (int j = 0; j < 2; j++)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+  {  
+    for (int k = 0; k < 2; k++)  
+    {  
+      threeDArray[i, j, k] = random.Next(1, 10);  
+    }  
+  }  
+}  
+
+foreach (var row in threeDArray)  
+{  
+  foreach (var element in row)  
+  {  
+    Console.Write($"({i}, {j}, {k}), ");  
+  }  
+  Console.WriteLine();  
+}  
+}
+}
---
+++ hometask/Task62_SpiralniyMassive/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07
+*/
+
+using System;
+
+class SpiralArray
+{
+static void Main()
+{
+int[,] array = new int[4, 4];
+
+    int startX = 0;
+    int startY = 0;
+    int endX = array.GetLength(0) - 1;
+    int endY = array.GetLength(1) - 1;
+
+    for (int i = 0; i < 16; i++)
+    {
+        array[startX, startY] = i;
+
+        if (startX < endX && startY < endY)
+        {
+            if (startX > 0) startX--;
+            else if (startY > 0)
+            {
+                startY--;
+                endX--;
+            }
+            else endY--;
+        }
+        else if (endY > startY)
+        {
+            endX--;
+        }
+
+        else
+        {
+            startX++;
+
+            if (endX > 1) endX--;
+
+            endY--;
+         }
+         }
+
+    static int[] intToArray(int num)
+    {
+    String holder = num.ToString();
+    int[] numbers = new int[Holder.ToString().Length]; 
+
+    for(int i=0;i<numbers.length;i++)
+    {
+        numbers[i] = Convert.toInt32(holder.CharAt(i));
+    }
+    return numbers;
+    }
+
+    static void PrintArray(int[,] array)
+    {
+        foreach (int[] row in array)
+        {
+            foreach (int num in row)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    PrintArray(array);
+}
+}
