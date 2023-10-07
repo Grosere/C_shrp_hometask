c48850f84a20fab308ff7c0017b3a8f2c609b730
Grosere <144502806+Grosere@users.noreply.github.com>
2023-10-07 22:59:39 +0300

    new hometask

---
+++ hometask/Task54_DvumernyMassive/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+using System;
+
+class Program
+{
+static void Main()
+{
+// Ввод размера массива и создание двумерного массива
+Console.WriteLine("Введите размер массива: ");
+string[] size = Console.ReadLine().Split();
+int rows = int.Parse(size[0]);
+int columns = int.Parse(size[1]);
+double[,] array = new double[rows, columns];
+
+    // Заполняем массив случайными числами и выводим на экран
+    Random rnd = new Random();
+    Console.WriteLine($"Исходный массив:");
+    for (int i=0; i<rows; i++)
+    {
+        for(int j=0; j<columns; j++)
+        {
+            array[i,j] = rnd.NextDouble() * 100;
+            Console.Write($"{array[i,j]:F2} ");
+        }
+        Console.WriteLine();
+    }
+    // Сортируем строки массива по убыванию и выводим результат
+for(int i=0; i<rows; i++)
+{
+double[] row = new double[columns];
+for(int j=0; j<columns; j++) row[j] = array[i,j];
+Array.Sort(row);
+for(int j=columns-1; j>=0; j--) array[i,j] = row[j];
+}
+Console.WriteLine($"Отсортированный по убыванию массив: ");
+for(int i=0; i<rows; i++)
+{
+for(int j=0; j<columns; j++) Console.Write($"{array[i,j]:F2} ");
+Console.WriteLine();
+}
+}
+}
---
+++ hometask/Task58_DveMatrix/Program.cs
@@ -0,0 +1,98 @@
+﻿/*
+Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+using System;
+
+public class MatrixMulti
+{
+public static void Main()
+{
+const int matrixSize = 3;
+Random rand = new Random();
+
+    // Создаем две матрицы
+    int[][] matrix1 = new int[matrixSize][];
+    int[][] matrix2 = new int[matrixSize][];
+
+    for (int i = 0; i < matrixSize; i++)
+    {
+        int[] row = new int[matrixSize];
+        matrix1[i] = row;
+        matrix2[i] = row;
+    }
+
+    // Заполняем матрицы случайными числами от 0 до 19
+    Console.WriteLine("Матрица 1:");
+    FillMatrix(matrix1, rand);
+    PrintMatrix(matrix1);
+    Console.WriteLine("");
+    Console.WriteLine("Матрица 2:");
+    FillMatrix(matrix2, rand);
+    PrintMatrix(matrix2);
+
+    // Находим произведение матриц
+    int[][] productMatrix = MatrixMulti.MatrixMultiplication(matrix1, matrix2);
+    PrintMatrix(productMatrix);
+
+}
+
+// Функция для заполнения матрицы случайными числами
+private static void FillMatrix(int[][] matrix, Random rand)
+{
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        for (int j = 0; j < matrix[i].Length; j++)
+        {
+            matrix[i][j] = rand.Next(20);
+        }
+    }
+}
+
+// Метод для печати матрицы
+private static void PrintMatrix(int[][] matrix)
+{
+    Console.WriteLine("[");
+    foreach(var row in matrix)
+    {
+      Console.WriteLine(String.Join(" ",row));
+     }
+    Console.WriteLine("]");
+}
+
+// Метод нахождения произведения матриц
+public static int[][] MatrixMultiplication(int[][] a, int[][] b)
+{
+    if (a.Length != b[0].Length)
+    {
+        throw new Exception("Некорректные размеры матриц!");
+    }
+    int n = a.Length;
+int[][] c = new int[n][];
+
+for (int i = 0; i < n; ++i)
+{
+c[i] = new int[b.Length];
+}
+
+    for (int i = 0; i < a.Length; ++i)
+    {
+        for (int j = 0; j < b.Length; ++j)
+        {
+            for (int k = 0; k < n; ++k)
+            {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    return c;
+}
+}
+
