16588fa2b7137c32e9763352e413d4f2ca07aef1
Grosere <144502806+Grosere@users.noreply.github.com>
2023-10-14 03:33:45 +0300

    Урок 9

---
+++ hometask/Task66_NaturalOtMdoN/Program.cs
@@ -0,0 +1,30 @@
+﻿/* 
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/ 
+
+using System;
+
+class SumOfNaturals
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите M и N:");
+        string input = Console.ReadLine();
+        string[] nums = input.Split(' ');
+        int m = int.Parse(nums[0]);
+        int n = int.Parse(nums[1]);
+
+        int sum = 0;
+        for (int i = m; i >= n; i--)
+        {
+            if (i % 2 == 0)
+            {
+                sum += i;
+            }
+        }
+
+        Console.WriteLine($"Сумма натуральных чисел от {m} до {n}: {sum}");
+    }
+}
